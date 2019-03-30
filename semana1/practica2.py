b0eccd93a7bdfea04ec0890303596f93fd58d6c3
erneslobo <49080682+erneslobo@users.noreply.github.com>
2019-03-30 12:13:23 -0600

    Add files via upload
    Practica semana 1

---
+++ semana1/practica2.py
@@ -0,0 +1,17 @@
+# Esta es la practica para escribir 2 lineas
+# en consola
+
+'''
+Comentario en multiples lineas
+util para explicar mas detalles
+'''
+
+print('Hola a todos!')
+print('buenos dias')
+
+#varias lineas en un solo print
+print('mi primera linea\n', 'mi segunda linea')
+
+#varias lineas en un solo print
+print('mi primera linea\nmi segunda linea')
+print('hola de nuevo')
---
+++ semana1/practica3.py
@@ -0,0 +1,23 @@
+#para revisar los tipos de datos
+
+# numero entero
+#definir un numero entero
+
+
+123456789
+mi_variable = 13456789
+
+print(mi_variable)
+
+#caso 1
+print('El valor de la variable llamada "mi_variable" es', mi_variable)
+
+#caso 2
+print("El valor de la variable llamada 'mi_variable' es", mi_variable)
+
+#caso 3
+print("El valor de la variable llamada \"mi_variable\" es", mi_variable)
+
+
+
+
